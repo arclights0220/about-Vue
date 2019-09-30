ae38b0c024520bd5b6ec5c3c0df1d003ee5c1ccf
mendox0220 <mendox0220@gmail.com>
2019-09-30 18:12:22 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <!--        <input type="text" v-model="str">-->
+        <!--        {{getReverseCom()}}-->
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+    export default {
+        //     data(){
+        //         return  {
+        //             str :  ""
+        //         }
+        //     },
+        //     methods : {
+        //         getReverseCom(){
+        //             console.log("adsf")
+        //             return this.str.split("").reverse().join("")
+        //         }
+        //     },
+        //
+        //     computed: {
+        //         getReverse(){
+        //             console.log("adsf")
+        //             return this.str.split("").reverse().join("")
+        //         }
+        //     }
+        // }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+        data(){
+        }
+    }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
